ec6b6109f8a006ea19c0667fe53c730567b20b54
Allan Wu <allan.wu@outlook.com.au>
2025-09-19 14:23:33 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// MAIN CODE
+// WIP
+
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include "startup.h"
+#include "ultrasonic.h"
+#include "linedetector.h"
+
+void setup() {
+  ;
+}
+
+void loop() {
+  ;
+}

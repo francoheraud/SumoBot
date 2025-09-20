dd3060a9439fc8b65455cb211ba7a257778bac10
Allan Wu <allan.wu@outlook.com.au>
2025-09-21 00:01:21 +0800

    Update ultrasonic.h

--- ultrasonic.h
+++ ultrasonic.h
@@ -1,12 +1,13 @@
+// Ultrasonic header file
+// Allan Wu (23810308)
+// 21 September 2025
 #ifndef ULTRASONIC_H
 #define ULTRASONIC_H
-
-// Ultrasonic header file
 
 // Global variable that can be referenced elsewhere, note that a negative value indicates no enemy found
 extern int ultrasonicDistanceNearestCm;
 
-// Set ultrasonicSerialEnable to 1 to enable printout to the serial monitor
+// Set ultrasonicSerialEnable to 1 using setupUltrasonicSensor(), to enable printout to the serial monitor
 extern int ultrasonicSerialEnable;
 
 void setupUltrasonicSensor(bool outputEnable = false);

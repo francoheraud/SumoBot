b654be6dbab38613d6da4f95b137742099e8ab8d
Allan Wu <allan.wu@outlook.com.au>
2025-09-21 22:14:55 +0800

    Moved switch statement into if block (prevChoice != currChoice)

--- startup.cpp
+++ startup.cpp
@@ -64,28 +64,28 @@ void userSelectFunction() {
       for (int i = 0; i < 6; i++) {
         tft.drawString(robotModeDescriptions[i], MENU_X_DATUM, MENU_Y_DATUM+20*(i+1));
       }
-    }
-    tft.setTextColor(HIGH_EMPHASIS_COLOUR, BACKGROUND_COLOUR);
-    tft.drawString(robotModeDescriptions[currChoice], MENU_X_DATUM, MENU_Y_DATUM+20*(currChoice+1));
-    switch (currChoice) {
-    case (0):
-      modeOfOperation = DEBUG_MODE;
-      break;
-    case (1):
-      modeOfOperation = TEST_1_DRIVE_STRAIGHT;
-      break;
-    case (2):
-      modeOfOperation = TEST_2_ROTATION;
-      break;
-    case (3):
-      modeOfOperation = TEST_3_TBD;
-      break;
-    case (4):
-      modeOfOperation = TEST_4_TBD;
-      break;
-    case (5):
-      modeOfOperation = COMPETITION_MODE;
-      break;
+      tft.setTextColor(HIGH_EMPHASIS_COLOUR, BACKGROUND_COLOUR);
+      tft.drawString(robotModeDescriptions[currChoice], MENU_X_DATUM, MENU_Y_DATUM+20*(currChoice+1));
+      switch (currChoice) {
+        case (0):
+          modeOfOperation = DEBUG_MODE;
+          break;
+        case (1):
+          modeOfOperation = TEST_1_DRIVE_STRAIGHT;
+          break;
+        case (2):
+          modeOfOperation = TEST_2_ROTATION;
+          break;
+        case (3):
+          modeOfOperation = TEST_3_TBD;
+          break;
+        case (4):
+          modeOfOperation = TEST_4_TBD;
+          break;
+        case (5):
+          modeOfOperation = COMPETITION_MODE;
+          break;
+      }
     }
     tft.setTextColor(PRIMARY_TEXT_COLOUR, BACKGROUND_COLOUR);
     prevChoice = currChoice;

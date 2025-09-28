819be57a4d16c3aa7298e07fef57353e1b0d69ea
Allan Wu <allan.wu@outlook.com.au>
2025-09-28 11:26:28 +0800

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -1,6 +1,15 @@
 #ifndef MOTORDRIVER_H
 #define MOTORDRIVER_H
+#include <Arduino.h>
 
-// Motor driver header file
+enum SpeedLevel { MIN, MEDIUM, MAX };
+enum Direction { FORWARD, REVERSE, LEFT, RIGHT, ROTATE_CW, ROTATE_CCW };
+
+void initMotors(int in1A, int in2A, int pwmA, int encA,
+                int in1B, int in2B, int pwmB, int encB);
+
+void move(Direction dir, SpeedLevel speed);
+void stopMotors();
+void updateMotors(); // PID loop, called continuously
 
 #endif

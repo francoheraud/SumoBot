28e41eaa59122c793562696420635fead16a7d22
Allan Wu <allan.wu@outlook.com.au>
2025-10-10 16:33:27 +0800

    Set default encoding value to 16
    Ensure encoding has been initialised

--- SumoBot/src/LineDetectors.cpp
+++ SumoBot/src/LineDetectors.cpp
@@ -3,10 +3,10 @@
 
 void detectLine(int lineDetectorAnalogPin, Line_t *detectors) // Pin must support ADC
 {
-    int binarycode[4]; // For debug purposes
+    int binarycode[4]; // For debug purposes we can check the values here
     int analogValue = analogRead(lineDetectorAnalogPin);
 	int *ptrs[4] = {&detectors->bottomRight, &detectors->bottomLeft, &detectors->topRight, &detectors->topLeft};
-    static int encoding;
+    int encoding = 16; // Defaults to '1111'
     for (int i = 0; i < 16; i++) {
         if (analogValue < ADCLookup[i]) {
             encoding = i;
